9a83edcd330e046f3e10d329398835a9a5523c52
egor5q <31898567+egor5q@users.noreply.github.com>
2018-09-26 21:39:31 +0300

    Update humanacts.py

--- humanacts.py
+++ humanacts.py
@@ -1,1 +1,11 @@
-
+def actfind(human, currenttime):
+  if human['age']<=22:
+    
+  elif human['age']<=35:
+    pass
+  elif human['age']<=60:
+    pass
+  elif human['age']<=85:
+    pass
+  elif human['age']<=110:
+    pass
