8f12998420defdbcb84983ae743ae087614a0c2e
egor5q <31898567+egor5q@users.noreply.github.com>
2018-09-27 10:38:04 +0300

    Update humanacts.py

--- humanacts.py
+++ humanacts.py
@@ -9,7 +9,7 @@ citytime=db.citytime
 def actfind(human, year, month, day, hour, minute):
   if human['age']<=22:
     if hour>=22 or hour<=5:
-      if human['athome']==0:
+      if human['variables']['athome']==0:
        if len(human['variables']['friends'])>0:
          if human['variables']['mood']>=500:
            if human['variables']['seer']!=None:
@@ -28,15 +28,18 @@ def actfind(human, year, month, day, hour, minute):
         gohome(human)          
       else:
         homework(human)
-    if hour>=6 and hour<=8:
-      if human['variable']['student']==1:
-        if human['variables']['mood']>=200 and human['diligence']>=350:
-          preparetostudy(human)
+    elif hour>=6 and hour<=8:
+      if human['variable']['student']==1 or human['variable']['worker']==1:
+        if human['variables']['mood']>=150 and human['diligence']>=230:
+          preparetowork(human)
         else:
           if random.randint(1,100)<=50:
             relax(human)
           else:
-            preparetostudy(human)
+            preparetowork(human)
+      else:
+        tryfindwork(human)
+          
   elif human['age']<=35:
     pass
   elif human['age']<=60:
@@ -46,13 +49,64 @@ def actfind(human, year, month, day, hour, minute):
   elif human['age']<=110:
     pass
 
- 
-def actend(human):
+def foundwork(human):
+  x=((human['attentiveness']/60)+(human['diligence']/60))*(human['luck']/385)
+  z=random.randint(1,100)
+  if z<=x:
+    foundedwork(human)
+    humans.update_one({'id':human['id']},{'$inc':{'variables.mood':55}})
+  else:
+    humans.update_one({'id':human['id']},{'$inc':{'variables.mood':-14}})
+    
   
+def foundedwork(human):
+  humans.update_one({'id':human['id']},{'$set':{'variables.worker':1}})
+  
+  
+def tryfindwork(human):
+  t=threading.Timer(180, actend, args=[human])
+  t.start()
+  t=threading.Timer(180, foundwork, args=[human])
+  t.start()
+  x=int(8/((human['happy']/550)*(human['diligence']/420)))
+  humans.update_one({'id':human['id']},{'$set':{'variables.acting':1}})
+  humans.update_one({'id':human['id']},{'$inc':{'variables.mood':-x}})
+  
+  
+def homework(human):
+ if human['variables']['homeworkready']==0 and human['variable']['student']==1:
+  if human['diligence']>=350 and human['variables']['mood']>=320:
+    if random.randint(1,100)<=95:
+      dohomework(human)           # Уроки
+    else:
+      relax(human)
+  else:
+    if random.randint(1,100)<=45:
+      dohomework(human)
+    else:
+      relax(human)
+ else:
+  relax(human)
+    
+def relax(human):
+  t=threading.Timer(30, actend, args=[human])
+  t.start()
+  humans.update_one({'id':human['id']},{'$set':{'variables.acting':1}})
+  humans.update_one({'id':human['id']},{'$inc':{'variables.mood':10}})
+    
+    
+  
+def actend(human):
+  humans.update_one({'id':human['id']},{'$set':{'variables.acting':0}})
+  
+def tohome(human):
+  humans.update_one({'id':human['id']},{'$set':{'variables.athome':1}})
 
 def gohome(human):
   t=threading.Timer(60, actend, args=[human])
   t.start()
+  f=threading.Timer(60, tohome, args=[human])
+  f.start()
   humans.update_one({'id':human['id']},{'$set':{'variables.acting':1}})
   
   
