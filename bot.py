186baaf7727b51186c5e40df00a139807e0deeb0
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-01 15:46:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,11 +15,8 @@ bot = telebot.TeleBot(token)
 
 client1=os.environ['database']
 client=MongoClient(client1)
-db=client.worldseer
+db=client.everlastingsummer
 users=db.users
-ya=users.find({})
-for ids in ya:
-  print(ids['name'])
 
 symbollist=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
            'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
