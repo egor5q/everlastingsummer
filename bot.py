aeea87c655d47dc3937792a60cd60d200dc2dd44
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-01 15:55:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,11 @@ client1=os.environ['database']
 client=MongoClient(client1)
 db=client.everlastingsummer
 users=db.users
+x=0
+ss=users.find({})
+for ids in ss:
+        x+=1
+print(str(x))
 
 symbollist=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
            'а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
