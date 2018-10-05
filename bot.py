7373d06578024ac29c2fa1e9b706e54704d64052
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-05 13:35:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -505,11 +505,11 @@ def eveninggames():
                          'у нас по плану придуманная Электроником карточная игра. [Электроник](https://t.me/ES_ElectronicBot), '+\
                          'дальше расскажешь ты.', 'markdown'])
         t.start()
-        time.sleep(4)
+        time.sleep(4.5)
         electronic.send_chat_action(-1001351496983,'typing')
-        t=threading.Timer(3, sendmes, args=[electronic, 'Есть, Ольга Дмитриевна!', None])
+        t=threading.Timer(2, sendmes, args=[electronic, 'Есть, Ольга Дмитриевна!', None])
         t.start()
-        time.sleep(1)
+        time.sleep(3)
         electronic.send_chat_action(-1001351496983,'typing') 
         t=threading.Timer(10, sendmes, args=[electronic, 'Итак. Правила игры просты: надо выиграть, собрав на руке более сильную '+\
                                             'комбинацию, чем у соперника. Процесс игры заключается в том, что соперники поочереди '+\
@@ -520,9 +520,9 @@ def eveninggames():
                                             'такой же ход повторяется со стороны соперника. Всего каждый участник делает 3 хода, '+\
                                             'и после этого оба игрока вскрываются...', None])
         t.start()
-        time.sleep(11)
+        time.sleep(12)
         electronic.send_chat_action(-1001351496983,'typing') 
-        t=threading.Timer(3, sendmes, args=[electronic, 'Что смешного? Ладно, неважно. Все поняли правила? Отлично! Для '+\
+        t=threading.Timer(5, sendmes, args=[electronic, 'Что смешного? Ладно, неважно. Все поняли правила? Отлично! Для '+\
                                               'регистрации в турнире нужно подойти ко мне, и сказать: "`Хочу принять участие в турнире!`". '+\
                                               'Регистрация заканчивается через 20 минут!', 'markdown'])
         t.start()
