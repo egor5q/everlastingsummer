d2fac61f37811f76bb9cb803e39ac3e5f1539ec1
egor5q <31898567+egor5q@users.noreply.github.com>
2019-06-27 16:11:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from emoji import emojize
 from telebot import types
 from pymongo import MongoClient
 import traceback
-import re
+#import re
 
 
 from requests.exceptions import ReadTimeout
@@ -2899,44 +2899,44 @@ def delusersevent(m):
     world.send_message(m.chat.id, 'success')
 
 
-@world.message_handler(commands=['start_event'])
-def starteventt(m):
-    if m.from_user.id == 441399484:
-        for ids in thunder_variables.find({}):
-            thunder_variables.remove({'name': ids['name']})
-        thunder_variables.insert_one(createvar('semen_1choice', None))
-        event_thunder_in_paradise()
-        if len(m.text.split()) == 2:
-            try:
-                event_thunder_in_paradise(polunin_pidor=True)
-            except:
-                bot.send_message(mainchat, 'Полунин облажался')
-                world.send_message(mainchat, traceback.format_exc())
+#@world.message_handler(commands=['start_event'])
+#def starteventt(m):
+#    if m.from_user.id == 441399484:
+#        for ids in thunder_variables.find({}):
+#            thunder_variables.remove({'name': ids['name']})
+#        thunder_variables.insert_one(createvar('semen_1choice', None))
+#        event_thunder_in_paradise()
+#        if len(m.text.split()) == 2:
+#            try:
+#                event_thunder_in_paradise(polunin_pidor=True)
+#            except:
+#                bot.send_message(mainchat, 'Полунин облажался')
+#                world.send_message(mainchat, traceback.format_exc())
+#
 
-
-def createvar(name, value):
-    return {
-        'name': name,
-        'value': value
-    }
-
+#def createvar(name, value):
+#    return {
+#        'name': name,
+#        'value': value
+#    }
+#
 
 #from events import Event  # этот импорт должен быть тут, чтобы избежать ошибок
 #from events.scenaries import grom  # как и этот
 
 
-def event_thunder_in_paradise(polunin_pidor=False):
-    actives = ['semen', 'pioner', '']
-    pi_user = thunder.find_one({'pioner': 'pioner'})
-    se_user = thunder.find_one({'pioner': 'semen'})
-    if polunin_pidor:
-        event = Event(mainchat, grom)
-        event.add_user(pi_user['id'], 'pioner')
-        event.add_user(se_user['id'], 'semen')
-        return
-    t = threading.Thread(target=pi_sends, args=[pi_user])
-    t.start()
-    for ids in thunder.find({}):
-        if ids['pioner'] != 'pioner':
-            world.send_message(ids['id'], 'Ваш временной промежуток ещё не настал. Ожидайте, история началась...')
-
+#def event_thunder_in_paradise(polunin_pidor=False):
+#    actives = ['semen', 'pioner', '']
+#    pi_user = thunder.find_one({'pioner': 'pioner'})
+#    se_user = thunder.find_one({'pioner': 'semen'})
+#    if polunin_pidor:
+#        event = Event(mainchat, grom)
+#        event.add_user(pi_user['id'], 'pioner')
+#        event.add_user(se_user['id'], 'semen')
+#        return
+#    t = threading.Thread(target=pi_sends, args=[pi_user])
+#    t.start()
+#    for ids in thunder.find({}):
+#        if ids['pioner'] != 'pioner':
+#            world.send_message(ids['id'], 'Ваш временной промежуток ещё не настал. Ожидайте, история началась...')
+#
